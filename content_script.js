9ab03bec29bcd90dea9a243c49f98403ec3ca590
OsiPog <osibluber@protonmail.com>
2022-06-23 20:14:42 +0200

    added condition to check if the view type is 'overview' and not show anything when view type is 'details'

--- content_script.js
+++ content_script.js
@@ -3,7 +3,17 @@ function round(num, digits=0) {
 }
 
 function updateAverage() {
+	// The timeout is set because the site has an internal loader.
 	setTimeout(function() {
+		// Getting the view-type of the marks as this has an impact on how to get the marks
+		// from the website.
+
+		// This is the checkbox radio element for the view type "overview".
+		let overview = document.querySelector("div.custom-control>input").checked;
+		
+		// Temporarily until the view type "Details" is finished
+		if (!overview) return;
+		
 		// Getting all subject divs
 		let all_subject_divs = document.querySelectorAll("div.card-body>div");
 		
@@ -12,19 +22,25 @@ function updateAverage() {
 		
 		for (let subject_div of all_subject_divs) {
 			
-			if (subject_div.querySelector("p")) continue; // This <p> element will only be present if the subject has no marks yet.
+			// This <p> element will only be present if the subject has no marks yet.
+			if (subject_div.querySelector("p")) continue; 
 
 			let subject_label = subject_div.querySelector("h2");
-			let mark_type_td_list = subject_div.querySelectorAll("tr>td");
+			
+			let mark_type_list = subject_div.querySelectorAll("tr>td");
 			
 			let average = 0;
-			// Only going through the first two because there should be only two types of marks (exam, others).
-			let len = Math.min(2, mark_type_td_list.length-1);
+			
+			// Only going through the first two because there should be only two types of 
+			// marks (exam, others).
+			let len = Math.min(2, mark_type_list.length-1);
 			for (let i=0;i<len;i++) {
 				let half_average = 0;
-				let marks = mark_type_td_list[i].querySelectorAll("div>span");
 
+				let marks = mark_type_list[i].querySelectorAll("div>span");
+				
 				for (let mark of marks) {
+					
 					half_average += parseInt(mark.innerHTML);
 				}
 				half_average /= marks.length;
@@ -48,7 +64,7 @@ function updateAverage() {
 		// putting the overall average next to the half year text
 		overall_average = overall_average/overall_av_len;
 		
-		let halfyear_selected = document.querySelector("nav>a.active")
+		let halfyear_selected = document.querySelector("nav>a.active");
 		
 		let overall_average_span = halfyear_selected.querySelector("span[bs-durchschnitt]"); 
 		if (!overall_average_span) {
@@ -57,16 +73,20 @@ function updateAverage() {
 			overall_average_span.setAttribute("bs-durchschnitt", "");
 		}
 		overall_average_span.innerHTML = "∅" + overall_average.toFixed(2);
-	}, 500);
+	}, 750);
 }
 
 function clickedOnBody() {
 	if (window.location.href.includes("grades")) { 
-		// The timeout is set because the site has an internal loader.
-		setTimeout(updateAverage, 500);
+		updateAverage();
 	}
 }
 
 
 
-document.body.addEventListener("click", clickedOnBody)
+document.body.addEventListener("click", clickedOnBody);
+
+// This will be execute after a site loaded.
+// It exists for the case that the current site is already the right one and now click has to 
+// be made
+clickedOnBody();

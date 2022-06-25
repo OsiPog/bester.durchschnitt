629e14b78ed6f8ccd0f783b28854213d31464010
OsiPog <osibluber@protonmail.com>
2022-06-25 17:42:33 +0200

    added option for user to decide which columns are exams or other marks, it can also be selected to ignore a column

--- content_script.js
+++ content_script.js
@@ -1,6 +1,9 @@
 // This is not a very elegant way to figure out if a column is for exam marks but as
-// I don't have an API nor does the Website provide any information about it.
-let EXAM_STRINGS = ["KL", "Klausur", "KA", "Kl", "Klassenarbeit"];
+// I don't have an API nor does the Website provide any information about it I have to
+// do it like this.
+let EXAM_STRINGS = ["KL", "Klausur", "KA", "Kl", "Klassenarbeit", "Ka"];
+
+let SELECT_OPTIONS = ["Sonstige", "KA/Klausur", "ignorieren"];
 
 function round(num, digits=0) {
 	return Math.round(num * 10 ** digits)/(10 ** digits);
@@ -13,7 +16,8 @@ function checkForExamString(string) {
 	return false;
 }
 
-function updateAverage() {
+function updateAverage(delay) {
+	console.log("update");
 	// The timeout is set because the site has an internal loader.
 	setTimeout(function() {
 		// Getting the view-type of the marks as this has an impact on how to get the marks
@@ -35,34 +39,71 @@ function updateAverage() {
 
 			let subject_label = subject_div.querySelector("h2");
 			console.log(subject_label.innerHTML);
-			let headers = subject_div.querySelectorAll("tr>th");
+	
 			let mark_lists = subject_div.querySelectorAll("tr>td");
+			let headers = subject_div.querySelectorAll("tr>th");
 			
-			createCheckboxes();
+			// Creating the dropdown menus and updating columns.
+			let select_states = new Array();
+			
+			for (let i = 0;i < headers.length-1; i++) {
+				let select = headers[i].querySelector("select[bs-durchschnitt]");
+				if (!select) {
+					select = document.createElement("select");
+					select.setAttribute("name", "mark-type");
+					select.setAttribute("bs-durchschnitt","");
+					select.setAttribute("style", "font-size:0.85em");
+					
+					for(let i = 0;i < SELECT_OPTIONS.length; i++) {
+						let opt = document.createElement("option");
+						opt.setAttribute("value", String(i));
+						opt.innerHTML = SELECT_OPTIONS[i];
+						select.appendChild(opt);
+					}
+					
+					if (checkForExamString(headers[i].innerHTML)) {
+						select.value = "1";
+					}
+					
+					headers[i].innerHTML += "<br>";
+					headers[i].appendChild(select);
+				}
+				
+				// Keeping track of the values for the calculations.
+				select_states.push(select.value);
+				
+				// If the dropdown menu is set to "ignore" make it visible.
+				let style = "";
+				if (select.value === "2") {
+					style = "color:#A0A0A0 !important";
+				}
+				
+				headers[i].setAttribute("style",style);
+				mark_lists[i].setAttribute("style",style);
+			}
+
 
 			let average_exams = 0;
 			let exams_amount = 0;
 			
 			let average_others = 0;
 			let others_amount = 0;
+			console.log(select_states);
 			
-			
-			for (let i = 0;i < headers.length-1; i++) { // The last column is empty
+			for (let i = 0;i < headers.length; i++) { // The last column is empty
+				
+				if (select_states[i] === "2") continue;
 				
 				let mark_elements = mark_lists[i].querySelectorAll("div>span");
-				
-				let is_exams = checkForExamString(headers[i].innerHTML);
-				
-				console.log(i);
-				console.log(is_exams);
-				
+
 				for (let mark_element of mark_elements) {
 					let mark = Number(mark_element.innerHTML)
 					
 					// Sometimes tests which haven't been attended are marked as "-".
+					console.log(mark_element);
 					if (Number.isNaN(mark)) continue;
 					
-					if (is_exams) {
+					if (select_states[i] === "1") {
 						average_exams += mark;
 						exams_amount++;
 					}
@@ -70,18 +111,21 @@ function updateAverage() {
 						average_others += mark;
 						others_amount++;
 					}
+					
+					console.log(average_exams, average_others);
 				}
 			}
-			let average;
-			
-			average_others /= others_amount;
+			let average = 0;
+			if (others_amount !== 0) {
+				average_others /= others_amount;
+				average += average_others;
+			}
 			if (exams_amount !== 0) {
 				average_exams /= exams_amount;
-				average = (average_exams + average_others)/2
+				average += average_exams;
 			}
-			else {
-				average = average_others;
-			}
+			
+			average /= (others_amount !== 0) + (exams_amount !== 0); // true = 1, false = 0
 			
 			let average_span = subject_label.querySelector("span[bs-durchschnitt]"); 
 			if (!average_span) {
@@ -89,10 +133,15 @@ function updateAverage() {
 				subject_label.appendChild(average_span);
 				average_span.setAttribute("bs-durchschnitt", "");
 			}
-			average_span.innerHTML = "∅" + average.toFixed(2);
 			
-			overall_av_len += 1;
-			overall_average += round(average); 
+			if (!Number.isNaN(average)) {
+				average_span.innerHTML = "∅" + average.toFixed(2);
+				overall_average += round(average);
+				overall_av_len++;
+			}
+			else {
+				subject_label.removeChild(average_span);
+			}
 		}
 		
 		// putting the overall average next to the half year text
@@ -107,20 +156,36 @@ function updateAverage() {
 			overall_average_span.setAttribute("bs-durchschnitt", "");
 		}
 		overall_average_span.innerHTML = "∅" + overall_average.toFixed(2);
-	}, 750);
+	}, delay);
 }
-
-function clickedOnBody() {
+function clicked(delay = 0) {
 	if (window.location.href.includes("grades")) { 
-		updateAverage();
+		updateAverage(delay);
 	}
 }
 
+function clickedOnBody() {
+	clicked(500);
+}
 
+function clickedOnCard() {
+	clicked(10);
+}
 
-document.body.addEventListener("click", clickedOnBody);
+function createEventListeners() {
+	let sidebar = document.querySelector("div.sidebar-sticky");
+	sidebar.addEventListener("click", clickedOnBody);
+	let view_button = document.querySelector("div#btn-radios-view");
+	view_button.addEventListener("click", clickedOnBody);
+
+	let card = document.querySelector("div.card-body");
+	card.addEventListener("click", clickedOnCard);
+}
+
+// Add click event listeners
+setTimeout(createEventListeners, 1000);
 
 // This will be execute after a site loaded.
-// It exists for the case that the current site is already the right one and now click has to 
+// This exists for the case that the current site is already the right one and no click has to 
 // be made
-clickedOnBody();
+clicked(1000);
